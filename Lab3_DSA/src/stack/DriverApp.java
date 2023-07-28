149d4ebddda6226088653a8cdca103f1eed6362b
KranthiGokul <123145563+KranthiGokul@users.noreply.github.com>
2023-07-28 13:58:56 +0530

    Add files via upload

---
+++ Lab3_DSA/src/stack/DriverApp.java
@@ -0,0 +1,13 @@
+package stack;
+
+public class DriverApp {
+public static void main(String[] args) {
+	String input = "{[()]}";
+	MyStack ms = new MyStack();
+	Boolean result = ms.checkBracket(input);
+	if(result == true)
+		System.out.println("The entered String has Balanced Brackets");
+	else
+		System.out.println("The entered Strings do not contain Balanced Brackets");
+}
+}
